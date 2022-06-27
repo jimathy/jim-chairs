2940ee346b49308d67530da01332d70912736a39
Jim Shield <gothicjim@gmail.com>
2022-06-27 16:44:31 +0100

    Rewrite/Optimization/Refactor/Exploit Fix - v1.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,18 +1,10 @@
 name "Jim-Chairs"
 author "Jimathy"
-version "v1.0"
+version "v1.1"
 description "Jim-Chairs By Jimathy"
 fx_version "cerulean"
 game "gta5"
 
-client_scripts {
-    'client/*.lua'
-}
-
-server_scripts {
-    'server/*.lua'
-}
-
-shared_scripts {
-    'config.lua',
-}
+client_scripts { 'client/*.lua' }
+server_scripts { 'server/*.lua' }
+shared_scripts { 'config.lua', }
--- server/server.lua
+++ server/server.lua
@@ -1,12 +1,9 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-local chairs = {}
-
 CreateThread(function()
-	for i=1, 110, 1 do
-		chairs[#chairs+1] = "chair"..i
-	end
-    for k,v in pairs(chairs) do
-       QBCore.Functions.CreateUseableItem(v, function(source, item) TriggerClientEvent('jim-chairs:Use', source, item.name) end)
+	for i=1, 110, 1 do 
+		QBCore.Functions.CreateUseableItem("chair"..i, function(source, item)
+			TriggerClientEvent('jim-chairs:Use', source, item.name) 
+		end)
 	end
 end)
