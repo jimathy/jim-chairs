b3068e87b73d60e44c22fa30e7424781247801ff
Jim Shield <gothicjim@gmail.com>
2025-04-08 20:33:32 +0100

    2.0 beta commit

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,41 @@
 name "Jim-Chairs"
 author "Jimathy"
-version "1.5"
-description "Jim-Chairs By Jimathy"
+version "2.0"
+description "Portable Chair Item Script"
 fx_version "cerulean"
 game "gta5"
 lua54 'yes'
 
-shared_scripts { 'config.lua', 'shared/*.lua' }
-client_scripts { 'client/*.lua' }
-server_scripts { 'server/*.lua' }
+server_script '@oxmysql/lib/MySQL.lua'
+
+shared_scripts {
+    'config.lua',
+
+    -- Required core scripts
+    '@ox_lib/init.lua',
+    '@ox_core/lib/init.lua',
+
+    '@es_extended/imports.lua',
+
+    '@qbx_core/modules/playerdata.lua',
+
+    --Jim Bridge
+    '@jim_bridge/starter.lua',
+
+    'shared/*.lua'
+}
+
+client_scripts {
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/EntityZone.lua',
+    '@PolyZone/CircleZone.lua',
+    '@PolyZone/ComboZone.lua',
+    --'@warmenu/warmenu.lua',
+
+    'client.lua',
+}
+
+server_script 'server.lua'
+
+dependency 'jim_bridge'
